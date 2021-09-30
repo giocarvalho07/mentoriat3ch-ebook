b9ca54e2eb9751ac79c065732af5bf1cce28cf33
giocarvalho07 <giovannidecarvalhomendes@outlook.com>
2021-09-30 20:40:30 -0300

    ajuste no projeto

--- src/main/java/com/ebook/carreirat3ch/domain/CarreiraT3ch.java
+++ src/main/java/com/ebook/carreirat3ch/domain/CarreiraT3ch.java
@@ -42,7 +42,54 @@ public class CarreiraT3ch {
     @NotBlank
     private String telefone;
 
+    public CarreiraT3ch() {
+    }
 
+    public CarreiraT3ch(Long id, String nome, String sobrenome, String email, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.telefone = telefone;
+    }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
